3f81d450b9fa7cabcb6e0cd4675d4d21d6d00f96
sukanya-devi9 <yellapusukanyadevi@gmail.com>
2025-05-20 18:22:26 +0530

    Add or update djangoapp files

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -21,7 +21,7 @@ def get_request(endpoint, **kwargs):
     try:
         response = requests.get(request_url)
         return response.json()
-    except:
+    except requests.exceptions.RequestException as e:
         print("Network Exception occurred")
 
 
@@ -53,5 +53,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except requests.exceptions.RequestException as e:
         print("Network exception occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -52,7 +52,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
     data = json.loads(request.body)
     username = data["userName"]
     password = data["password"]
@@ -60,12 +60,12 @@ def registration(request):
     last_name = data["lastName"]
     email = data["email"]
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except requests.exceptions.RequestException as e:
         # If not,simply log this as user
         logger.debug("{} is new user".format(username))
     # if it is a new user
@@ -139,12 +139,12 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
-        except:
+            return JsonResponse({"status": 200, "data": response})
+        except  requests.exceptions.RequestException as e:
             return JsonResponse(
                 {"status": 401, "message": "Error in posting review"}
             )
